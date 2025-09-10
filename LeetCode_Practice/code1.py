4002e85100342a6100ac1f07b7346780f3141936
MackInSpace <amackey@lssu.edu>
2025-09-10 15:26:18 -0600

    leetcode3

--- LeetCode_Practice/code1.py
+++ LeetCode_Practice/code1.py
@@ -7,3 +7,13 @@
 """class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
         return nums + nums"""
+
+#Problem 1512
+"""class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j]:
+                    count += 1
+        return count"""
