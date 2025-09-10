c3c5d0bfca75eca34646f0c095e055081881a2fe
MackInSpace <amackey@lssu.edu>
2025-09-10 14:21:35 -0600

    leetcode2

--- LeetCode_Practice/code1.py
+++ LeetCode_Practice/code1.py
@@ -1,3 +1,9 @@
-class Solution:
+#Problem 2235
+"""class Solution:
     def sum(self, num1: int, num2: int) -> int:
-        return num1 + num2
+        return num1 + num2"""
+
+#Problem 1929
+"""class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums"""
