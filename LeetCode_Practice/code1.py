36558c65b2099108e7df6c3879a394d3bfd52ddb
MackInSpace <amackey@lssu.edu>
2025-09-18 13:55:11 -0600

    leetcode 27

--- LeetCode_Practice/code1.py
+++ LeetCode_Practice/code1.py
@@ -118,3 +118,13 @@ if __name__ == "__main__":
          return True"""
 
 #Problem 27
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
