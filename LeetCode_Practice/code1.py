a24ea1ffc79eb44e5f563f3404c72077cef3ccc6
MackInSpace <amackey@lssu.edu>
2025-09-18 13:53:08 -0600

    leetcode problem 36

--- LeetCode_Practice/code1.py
+++ LeetCode_Practice/code1.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     print("All tests passed.")"""
 
 #Problem 15
-class Solution:
+"""class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         res = []
@@ -96,4 +96,25 @@ class Solution:
                     left += 1
                     right -= 1
         
-        return res
+        return res"""
+
+#Problem 36
+"""class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+         rows = [set() for _ in range(9)]
+         cols = [set() for _ in range(9)]
+         boxes = [set() for _ in range(9)]
+
+         for i in range(9):
+             for j in range(9):
+                 num = board[i][j]
+                 if num != ".":
+                     box_index = (i // 3) * 3 + j // 3
+                     if num in rows[i] or num in cols[j] or num in boxes[box_index]:
+                         return False
+                     rows[i].add(num)
+                     cols[j].add(num)
+                     boxes[box_index].add(num)
+         return True"""
+
+#Problem 27
