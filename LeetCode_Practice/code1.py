e33017ab92da55fde8fa8fa06cad7bc0fb458253
MackInSpace <amackey@lssu.edu>
2025-09-30 11:22:57 -0600

    leetcode 41

--- LeetCode_Practice/code1.py
+++ LeetCode_Practice/code1.py
@@ -164,7 +164,7 @@ if __name__ == "__main__":
         return result"""
 
 #Problem 11
-class Solution:
+"""class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = len(height) - 1
@@ -179,4 +179,16 @@ class Solution:
             else:
                 right -= 1
 
-        return max_area
+        return max_area"""
+
+#Problem 41
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            while nums[i] > 0 and nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+        return n + 1
