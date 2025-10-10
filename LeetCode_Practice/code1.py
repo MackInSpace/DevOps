ef4bbe9b8c706ff77c311da6a7fd31304f8e841c
MackInSpace <amackey@lssu.edu>
2025-10-10 10:53:09 -0600

    leetcode 2

--- LeetCode_Practice/code1.py
+++ LeetCode_Practice/code1.py
@@ -210,9 +210,31 @@ if __name__ == "__main__":
         return dp[0][0]"""
 
 #Problem 1
-class Solution:
+"""class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
+                    return [i, j]"""
+
+#Problem 2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            carry += val1 + val2
+            curr.next = ListNode(carry % 10)
+            carry //= 10
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
